33cf0ade2acbfb8eaea5621ec81bc7cc774e1421
unknown <Josiah Calderon>
2026-03-19 22:29:25 +0800

    Practice Set 4 Answers

--- practice_set4/ps4_prog03.py
+++ practice_set4/ps4_prog03.py
@@ -1,8 +1,8 @@
 numbers = []
 while True:
-    num = int(input("Please enter a number: "))
-    if num not in numbers:
-        print("Unique")
-    else:
-        print("Duplicate")
-    numbers.append(num)
+    try:
+        num = int(input("Enter a number: "))
+        numbers.append(num)
+    except ValueError:
+        break
+print(f"Highest number: {max(numbers)}")
